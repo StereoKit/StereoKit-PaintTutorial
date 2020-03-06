7dd981cd88af7bbe937055d72faac58958173f4e
Charles Phillips <charles@doublerebel.com>
2020-03-05 17:10:28 -0800

    [refactor model ui api] factor out File Picker and File IO

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using StereoKit;
 using StereoKit.Framework;
 
@@ -10,7 +9,6 @@ namespace StereoKitPaintTutorial
         static Painting    activePainting = new Painting();
         static PaletteMenu paletteMenu;
         static Pose        menuPose       = new Pose(new Vec3(0.4f, 0, -0.4f), Quat.LookDir(-1,0,1));
-        static string      defaultFolder  = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
         static void Main(string[] args)
         {
@@ -70,21 +68,16 @@ namespace StereoKitPaintTutorial
             // name and folder have been selected, it'll make a call to SavePainting with the
             // file's path name with the .skp extension.
             if (UI.Button("Save"))
-                FilePicker.Show(
-                    FilePickerMode.Save,
-                    defaultFolder,
-                    SavePainting,
-                    new FileFilter("Painting", "*.skp"));
+                TextToFromFileWithFilePicker.Save(activePainting.ToFileData());
 
             // And on that same line, we'll have a load button! This'll let the user pick out
             // any .skp files, and will call LoadPainting with the selected file.
             UI.SameLine();
             if (UI.Button("Load"))
-                FilePicker.Show(
-                    FilePickerMode.Open,
-                    defaultFolder,
-                    LoadPainting,
-                    new FileFilter("Painting", "*.skp"));
+                TextToFromFileWithFilePicker.Load((string text) =>
+                {
+                    activePainting = Painting.FromFile(text);
+                });
 
             // Clear is easy! Just create a new Painting object!
             if (UI.Button("Clear"))
@@ -98,12 +91,5 @@ namespace StereoKitPaintTutorial
             // And end the window!
             UI.WindowEnd();
         }
-
-        static void LoadPainting(string file)
-            => activePainting = Painting.FromFile(File.ReadAllText(file));
-        
-        static void SavePainting(string file)
-            => File.WriteAllText(file, activePainting.ToFileData());
-        
     }
 }
---
+++ TextToFromFileWithFilePicker.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using StereoKit.Framework;
+
+namespace StereoKitPaintTutorial
+{
+    class TextToFromFileWithFilePicker
+    {
+        static string defaultFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public static void Load(Action<string> callback)
+        {
+            FilePicker.Show(
+                FilePickerMode.Open,
+                defaultFolder,
+                callback,
+                new FileFilter("Painting", "*.skp"));
+        }
+
+        public static void Save(string fileData)
+        {
+            Action<string> callback = (string file) =>
+            {
+                File.WriteAllText(file, fileData);
+            };
+
+            FilePicker.Show(
+                FilePickerMode.Save,
+                defaultFolder,
+                callback,
+                new FileFilter("Painting", "*.skp"));
+        }
+
+    }
+}
